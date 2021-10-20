2b61490f887a88b77fe4c8365b138fefd58c75e7
Documenter.jl <documenter@juliadocs.github.io>
2021-10-20 19:55:17 +0000

    build based on 3f8d125

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackageKaining","category":"page"},{"location":"#DemoPackageKaining","page":"Home","title":"DemoPackageKaining","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackageKaining.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackageKaining]","category":"page"}]
+}
